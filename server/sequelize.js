fbef9d4bb74a29a6790c2727ef2917648e9af02a
Hyrum Sorensen <hyrumgary@gmail.com>
2022-08-15 16:40:20 -0600

    Initial Commit

---
+++ server/sequelize.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const {Sequelize} = require('sequelize')
+
+const {CONNECTION_STRING} = process.env
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
+
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
+module.exports = sequelize
